3a25b7f06b371b34b5d3552044054d00daea3c1c
Yevheniia <janelisd2@gmail.com>
2022-12-12 20:38:46 +0200

    update hw

---
+++ Yevheniia_Danshyna_HW18/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+C:/Users/admin/Documents/GitHub/Lesson_18_HW> mkdir Yevheniia_Danshyna_HW18
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> touch script.sh
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> mkdir Need_to_check
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> cd C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check
+
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check> cat>README.txt
+Прочитай мене!
+Read me!
+
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check> touch Text1.txt
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check> touch Text2.txt
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check> touch Text3.txt
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check> cd..
+
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> mkdir Checked
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> cp C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text1.txt C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Checked
+Скопировано файлов:         1.
+
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> mv C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text2.txt C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Checked
+Перемещено файлов:         1.
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18> rm C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text3.txt
+
+C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18>

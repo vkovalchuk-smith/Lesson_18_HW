262150e9e4fb782961d98b8c8a21ca3a819f3e1a
Yevheniia <janelisd2@gmail.com>
2022-12-12 22:08:30 +0200

    Change Danshyna

---
+++ Yevheniia_Danshyna_HW18/script.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#!/bin/bash
+$ mkdir Need_to_check
+$ cat> README.txt
+Прочитай мене
+$ touch Text1.txt Text2.txt Text3.txt
+$ mkdir checked
+$ cp C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text1.txt C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/checked
+$  mv C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text2.txt C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/checked
+$ rm C:/Users/admin/Documents/GitHub/Lesson_18_HW/Yevheniia_Danshyna_HW18/Need_to_check/Text3.txt[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D#!/[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C

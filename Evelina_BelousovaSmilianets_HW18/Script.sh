3197a4fee442d0f31ff45aa9d145c8336f8d7f6e
Evelina Belousova <belousovaevelina@gmail.com>
2022-12-15 00:53:55 +0200

    HW18
    HW18

---
+++ Evelina_BelousovaSmilianets_HW18/Script.sh
@@ -0,0 +1,13 @@
+cd Evelina_BelousovaSmilianets_HW18
+mkdir Need_to_check
+cd Need_to_check
+cat>README.txt
+Прочитай мене!
+cat>Text1.txt
+cat>Text2.txt
+cat>Text3.txt
+cd Evelina_BelousovaSmilianets_HW18
+mkdir Checked
+copy c:\Users\User\Documents\GitHub\Lesson_18_HW\Evelina_BelousovaSmilianets_HW18\Need_to_check\Text1.txt c:Users\User\Documents\GitHub\Lesson_18_HW\Evelina_BelousovaSmilianets_HW18\Checked
+move c:Users\User\Documents\GitHub\Lesson_18_HW\Evelina_BelousovaSmilianets_HW18\Need_to_check\Text2.txt c:Users\User\Documents\GitHub\Lesson_18_HW\Evelina_BelousovaSmilianets_HW18\Checked
+del c:Users\User\Documents\GitHub\Lesson_18_HW\Evelina_BelousovaSmilianets_HW18\Need_to_check\Text3.txt

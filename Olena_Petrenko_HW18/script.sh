db617aef98a36672537b2203ea088a75f0ceabe0
Olena Petrenko <120414253+O-l-e-n-a@users.noreply.github.com>
2022-12-14 03:41:07 +0200

    Create executable file script.txt

---
+++ Olena_Petrenko_HW18/script.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18 (main)
+$ mkdir Need_to_check
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Need_to_check (main)
+$ cat > README.txt
+Прочита мене!
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18/Need_to_check (main)
+$ touch Text1.txt Text2.txt Text3.txt
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18 (main)
+$ mkdir Cheked
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18/Need_to_check (main)
+$ cp C:/Users/1/Documents/GitHub/Lesson_18_HW/Olena_Petrenko_HW18/Need_to_check/text1.txt  C:/users/1/documents/github/lesson_18_HW/Olena_Petrenko_HW18/Cheked
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18/Need_to_check (main)
+$ mv C:/Users/1/Documents/GitHub/Lesson_18_HW/Olena_Petrenko_HW18/Need_to_check/Text2.txt C:/Users/1/Documents/GitHub/Lesson_18_HW/Olena_Petrenko_HW18/Checked
+
+1@DESKTOP-G5BKND9 MINGW64 ~/documents/github/lesson_18_HW/Olena_Petrenko_HW18/Need_to_check (main)
+$ rm C:/Users/1/Documents/GitHub/Lesson_18_HW/Olena_Petrenko_HW18/Need_to_check/Text3.txt
+
+
+
+
